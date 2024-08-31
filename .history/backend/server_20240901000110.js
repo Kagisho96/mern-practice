f4447feda86a7f0a13afcbd51e153d716b58c8b7
kagisho96 <kagishopitsi@gmail.com>
2024-09-01 00:19:11 +0200

    Goals controller and routes setuo

--- .history/backend/server_20240901000110.js
+++ .history/backend/server_20240901000110.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
+// const { errorHandler } = require('./middleware/errorMiddleware');
 // const connectDB = require('./config/db');
 const port = process.env.PORT || 8080;
 
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 // app.use('/api/users', require('./routes/userRoutes'));
